ad766a7df4e87bcd0e085f826799820e88728855
ricardo <ricshark@hotmail.com>
2021-11-06 16:15:42 -0300

    Deleting personnal key

--- Weather/Weather/WeatherApp/Constants.cs
+++ Weather/Weather/WeatherApp/Constants.cs
@@ -3,6 +3,6 @@
     public static class Constants
     {
         public static string OpenWeatherMapEndpoint = "https://api.openweathermap.org/data/2.5/weather";
-        public static string OpenWeatherMapAPIKey = "6007a44a8180a4e0904ada94cf78770c";
+        public static string OpenWeatherMapAPIKey = "INSERT YOUR KEY HERE";
     }
 }
